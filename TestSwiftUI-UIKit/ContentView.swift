c8fdd5cc85015059fa4a319924a00331b93c5905
Nikolay Eckert <nikolay-eckert@yandex.ru>
2021-03-02 20:20:53 +0700

    Init

---
+++ TestSwiftUI-UIKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSwiftUI-UIKit
+//
+//  Created by Nikulux on 02.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
